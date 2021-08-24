f9c8df760ce0b4d602eb19974472985e03f46de3
jp-96 <jp-96@outlook.jp>
2021-08-24 15:43:18 +0900

    python3 input()

---
+++ official/official_build3.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+source ../setup/install_docker.sh
+source ../setup/usedPort.sh
+
+MC_ROOT=/var/mc_official_data
+
+d_cmd=$(type -P docker)
+d_com_cmd=$(type -P docker-compose)
+
+if [ -z "$d_cmd" ]; then
+    install_docker
+else
+    echo "dockerは既にインストールされています。"
+fi
+
+if [ -z "$d_com_cmd" ]; then
+    install_docker_compose
+else
+    echo "docker-composeは既にインストールされています。"
+fi
+
+yes | sudo apt autoremove
+
+docker-compose rm -fs
+
+isUsedPort 80
+if [ $? -ne 0 ]; then
+    echo "80番ポートが使用中のため、インストール中止しました。"
+    exit 1
+fi
+
+sudo mkdir -p $MC_ROOT
+
+script_dir=$(cd $(dirname -- $0); pwd)
+
+python3 ${script_dir}/../setup/operator3.py $MC_ROOT .
+
+if [ $? -ne 0 ]; then
+    echo "Minecraftサーバーの起動を中止しました。"
+    exit 1
+fi
+
+# Minecraftコンテナを起動します。
+
+cd $script_dir
+docker-compose up -d
